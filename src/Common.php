06077bb5092ea9faedf3bb98c7f8bdfb4d276069
l3043Y <boreyc2013@gmail.com>
2024-11-20 23:28:23 +0700

    add AppVersion

--- src/Common.php
+++ src/Common.php
@@ -2,8 +2,23 @@
 
 namespace l3043y\Common;
 
-use l3043y\Common\Data\ApiResponse;
+class Common
+{
+    public static function appVersionIntVal(?string $version): int
+    {
+        if (is_null($version) || $version == '-') {
+            return PHP_INT_MAX;
+        }
+        $pattern = '/^\d{1,2}\.\d{1,2}\.\d{1,2}$/';
+        if ($version == '*' || !preg_match($pattern, $version)) {
+            return 0;
+        }
 
-class Common {
-
+        $versionNumList = explode('.', $version);
+        if (isset($versionNumList[2]) && intval($versionNumList[2]) < 10) {
+            $versionNumList[2] = str_pad(intval($versionNumList[2]), 2, '0', STR_PAD_LEFT);
+        }
+        $vnumber = implode('', $versionNumList);
+        return intval($vnumber);
+    }
 }
